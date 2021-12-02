09075e8d9494dd33df8b4e57df10257613f4356a
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2021-12-02 12:47:32 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,13 @@
 Lista = Array.from(window.location.href.split("/"))
 
 /// Removendo Pacientes das Telas Pacientes
-
 console.log(Lista.at(-1))
+
+if(JSON.parse(sessionStorage.getItem("nome")).lenght <1){
+    alu=[]
+    sessionStorage.setItem("nome",JSON.stringify(alu))
+}
+
 if (Lista.at(-1) === "Pacientes.html"){
     paciente = document.getElementById("boneco")
     paciente2 = document.getElementById("boneco2")
@@ -25,17 +30,24 @@ if (Lista.at(-1) === "Pacientes.html"){
 
 if (Lista.at(-1) === "Adicionar.html"){
     document.addEventListener("DOMContentLoaded", function() {
+        
         botao = document.getElementById("botao_ok")
 
         botao.addEventListener("click", function(event){
             // Conteudo da Mensagem
-            nome = document.getElementById("Nome_paciente").value
+            event.preventDefault()
+            lista = JSON.parse(sessionStorage.getItem("nome"))
+            console.log(lista)
+            nome = JSON.stringify(lista.push(document.getElementById("Nome_paciente").value))
             sessionStorage.setItem('nome',nome)
         })
     })
 }
 
 if (Lista.at(-1) === "Pacientes.html"){
+    if(sessionStorage.getItem("nome")=== null){
+        console.log("Não Existem Nomes no SessionStorage")
+    } else{
     /// Adiciona Nome colocado na Lista
     main = document.getElementById("lista_pacientes")
 
@@ -59,5 +71,6 @@ if (Lista.at(-1) === "Pacientes.html"){
     a.appendChild(h3)
 
     main.appendChild(a)
-    sessionStorage.clear()
+    // sessionStorage.clear()
     }
+}
